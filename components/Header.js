6992dee9871a34865b1592fef9b7cd12dfb57e83
Aatif Ahmad <b22ai002@iitj.ac.in>
2024-09-07 05:38:33 +0530

    five

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,17 @@ export default function Header() {
       <p className="text-center text-[#D4D4D4] p-2 md:p-3 bg-[#222222]">
         Aatif Portfolio - Visual Studio Code
       </p>
-      <p className="p-1 bg-[#2C2C2C]">
-        <ul className="flex gap-2 md:gap-3 text-[#D4D4D4] text-sm">
-          <li>File</li>
-          <li>Edit</li>
-          <li>Selection</li>
-          <li>View</li>
-          <li>Go</li>
-          <li>Run</li>
-          <li>Terminal</li>
-          <li>Help</li>
-        </ul>
-      </p>
+      {/* <p className=" bg-[#2C2C2C]"></p> */}
+      <ul className="flex gap-2 md:gap-3 text-[#D4D4D4] text-sm bg-[#2C2C2C] p-1">
+        <li>File</li>
+        <li>Edit</li>
+        <li>Selection</li>
+        <li>View</li>
+        <li>Go</li>
+        <li>Run</li>
+        <li>Terminal</li>
+        <li>Help</li>
+      </ul>
     </div>
   );
 }
